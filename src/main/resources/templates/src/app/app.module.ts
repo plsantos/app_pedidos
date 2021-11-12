4355c4de5dee4ef1dab1e40ad2dcc0bb791a167c
Paula Santos <paulagms@gmail.com>
2021-11-12 15:57:59 -0300

    refazendo pedido

--- src/main/resources/templates/src/app/app.module.ts
+++ src/main/resources/templates/src/app/app.module.ts
@@ -1,25 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
-
 import { MatTableModule } from '@angular/material/table';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { HomeComponent } from './home/home.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import {MatButtonModule} from '@angular/material/button';
 import { OrderListComponent } from './order-list/order-list.component';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
 import { PedidoComponent } from './pedido/pedido.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 
