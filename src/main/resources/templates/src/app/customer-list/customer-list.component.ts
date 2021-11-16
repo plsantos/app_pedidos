2f008fa2dc7093b8881101f1c221221c9439092f
alline.cardoso <migowskialline@gmail.com>
2021-11-16 12:15:19 -0300

    ts customerList refatorado para mostrar detalhes endereço

--- src/main/resources/templates/src/app/customer-list/customer-list.component.ts
+++ src/main/resources/templates/src/app/customer-list/customer-list.component.ts
@@ -3,30 +3,23 @@ import { Cliente } from '../model/cliente';
 import { CustomerService } from './../services/customer.service';
 import { Observable } from 'rxjs';
 
-
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
-  styleUrls: ['./customer-list.component.css']
+  styleUrls: ['./customer-list.component.css'],
 })
-
 export class CustomerListComponent implements OnInit {
-
-  cliente$:  Cliente[] = [];
-   displayedColumns = ['id', 'nome', 'tipo', 'documento', 'endereco', 'acoes'];
+  cliente$: Cliente[] = [];
+  displayedColumns = ['id', 'nome', 'tipo', 'documento', 'endereco', 'acoes'];
 
   constructor(private customerService: CustomerService) {
     //this.customer$ = this.customerService.findAll();
-
   }
 
-
-   ngOnInit(): void {
-    this.customerService.getClientes()
-     .subscribe(data => {
+  ngOnInit(): void {
+    this.customerService.getClientes().subscribe((data) => {
       this.cliente$ = data;
       console.log(data);
-
-   })
-   }
+    });
+  }
 }
