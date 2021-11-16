bb7dd05ab4b48f8c9296c13dd646cf7548dd184f
alline.cardoso <migowskialline@gmail.com>
2021-11-16 12:16:15 -0300

    service productList refatorado

--- src/main/resources/templates/src/app/services/product.service.ts
+++ src/main/resources/templates/src/app/services/product.service.ts
@@ -1,56 +1,33 @@
-// import { Injectable } from '@angular/core';
-// import { Product } from '../model/product';
-// import { HttpClient } from '@angular/common/http';
-
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ProductService {
-
-
-//   private readonly API = '/assets/product.json';
-
-
-//   constructor(public httpClient: HttpClient) { }
-
-//   findAll(){
-//     return this.httpClient.get<Product[]>(this.API);
-// }
-// }
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Produto } from '../model/produto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) {}
+  Url = 'http://localhost:8080/produto';
 
-  Url='http://localhost:8080/produto';
-
-  getProdutos(): Observable<any>{
+  getProdutos(): Observable<any> {
     return this.http.get<Produto[]>(this.Url);
   }
 
-  getProduto(id: string): Observable<any>{
+  getProduto(id: string): Observable<any> {
     return this.http.get<Produto>(this.Url + id);
   }
 
-  saveProduto(produto: Produto): Observable<any>{
+  saveProduto(produto: Produto): Observable<any> {
     return this.http.post<Produto>(this.Url, produto);
   }
 
-  editProduto(id: String, produto: Produto): Observable<any>{
+  editProduto(id: String, produto: Produto): Observable<any> {
     return this.http.put(this.Url + id, produto);
   }
 
-  deleteProduto(id: String): Observable<any>{
+  deleteProduto(id: String): Observable<any> {
     return this.http.delete(this.Url + id);
   }
-
 }
