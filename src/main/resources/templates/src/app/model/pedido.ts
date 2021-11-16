9d643d2c64d41b8be37c892c19209dc8fd436872
alline.cardoso <migowskialline@gmail.com>
2021-11-16 12:14:02 -0300

    componente orderList refatorado para receber api postman

--- src/main/resources/templates/src/app/model/pedido.ts
+++ src/main/resources/templates/src/app/model/pedido.ts
@@ -1,8 +1,11 @@
-export interface Pedido {
-  id:number;
-  cliente: String;
-  dataPedido: Date;
-  valorTotal: number;
-  situacao: boolean;
-}
+import { Cliente } from '../model/cliente';
 
+export class Pedido {
+  constructor(
+    public id: number,
+    public cliente: Cliente,
+    public data: Date,
+    public valorTotal: number,
+    public situacao: boolean
+  ) {}
+}
--- src/main/resources/templates/src/app/services/order.service.ts
+++ src/main/resources/templates/src/app/services/order.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Pedido } from '../model/pedido';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
+  constructor(private http: HttpClient) {}
 
-  private readonly API = '/assets/order.json';
+  Url = 'http://localhost:8080/pedido';
 
+  getPedidos(): Observable<any> {
+    return this.http.get<Pedido[]>(this.Url);
+  }
 
-  constructor(public httpClient: HttpClient) { }
+  getPedido(id: string): Observable<any> {
+    return this.http.get<Pedido>(this.Url + id);
+  }
 
-  findAll(){
-    return this.httpClient.get<Pedido[]>(this.API);
+  savePedido(pedido: Pedido): Observable<any> {
+    return this.http.post<Pedido>(this.Url, pedido);
+  }
+
+  editPedido(id: String, pedido: Pedido): Observable<any> {
+    return this.http.put(this.Url + id, pedido);
+  }
+
+  deletePedido(id: String): Observable<any> {
+    return this.http.delete(this.Url + id);
   }
 }
+
+
