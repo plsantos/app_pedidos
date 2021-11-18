02d81712a132066f6ca41153d7372b7ca6bac628
Giovanna <giovannamsantanaa@gmail.com>
2021-11-18 18:01:34 -0300

    Criando o service de ItensPedido

---
+++ src/main/resources/templates/src/app/services/item-order.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ItensPedido } from '../model/itensPedido';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemOrderService {
+
+  constructor(private http: HttpClient) { }
+
+  urlBase = "http://localhost:8080/itensPedido"
+
+  getItensPedido(){
+    return this.http.get<ItensPedido[]>(this.urlBase);
+  }
+
+
+}
