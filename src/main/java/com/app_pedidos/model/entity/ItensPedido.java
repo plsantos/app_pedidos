1eb11805c41214092b361e1a4616d2bea4a8f24d
natashabusnardo <natashabusnardo@hotmail.com>
2021-11-19 10:15:04 -0300

    Alterações classes Pedido e Itens pedido

--- src/main/java/com/app_pedidos/model/entity/ItensPedido.java
+++ src/main/java/com/app_pedidos/model/entity/ItensPedido.java
@@ -18,14 +18,7 @@ public class ItensPedido {
     private Long id;
     private int quantidadeProduto;
     @ManyToOne
-    @JoinColumn(name = "pedido_id")
-    private Pedido pedido;
-    @ManyToOne
     @JoinColumn(name = "produto_id")
     private Produto produto;
-
-
-
-
 
 }
