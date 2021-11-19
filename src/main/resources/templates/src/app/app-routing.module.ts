f74dc3041da1cd3ef58dea6f1d0ded78fd59c4ad
byanca <byanca.calixto@dcx.ufpb.br>
2021-11-19 11:54:04 -0300

    ajuste no app-routing.module

--- src/main/resources/templates/src/app/app-routing.module.ts
+++ src/main/resources/templates/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { OrderListComponent } from './order-list/order-list.component';
 import { PedidoComponent } from './pedido/pedido.component';
 import { CustomerFormComponent } from './customer-form/customer-form.component';
 import {CadastroProdutoComponent} from './cadastro-produto/cadastro-produto.component';
+import { ItemOrderComponent } from './item-order/item-order.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 
 const routes: Routes = [
@@ -18,26 +20,14 @@ const routes: Routes = [
   { path: 'pedido', component: PedidoComponent },
   { path: 'customerForm', component: CustomerFormComponent },
   { path: 'cadastroProduto', component: CadastroProdutoComponent},
-=======
-import { ItemOrderComponent } from './item-order/item-order.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
+  { path: 'home', component: HomeComponent },
+  { path: 'item-order', component: ItemOrderComponent},
+  { path: 'header',component: HeaderComponent},
+  { path: 'footer',component: FooterComponent}
 
-const routes: Routes = [
-  {path: 'home', component: HomeComponent },
-  {path: 'item-order', component: ItemOrderComponent},
-  {path: 'header',component: HeaderComponent},
-  {path: 'footer',component: FooterComponent}
->>>>>>> feature/byanca.calixto
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-<<<<<<< HEAD
 export class AppRoutingModule {}
-=======
-export class AppRoutingModule { }
-
->>>>>>> feature/byanca.calixto
