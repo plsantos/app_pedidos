5adf766bed78ff181d51be4f13038f360c92b1f6
Caroline Souza <loh_jg@hotmail.com>
2021-11-22 16:25:44 -0300

    perfis de dev e teste

---
+++ src/test/java/com/app_pedidos/tests/repositories/ProdutoRepositoryTests.java
@@ -0,0 +1,8 @@
+package com.app_pedidos.tests.repositories;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest //executa o teste carregando somente os componentes do JPA
+public class ProdutoRepositoryTests {
+	
+}
