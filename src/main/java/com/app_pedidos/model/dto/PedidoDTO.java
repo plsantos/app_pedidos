60fb40b1e177d8245a5cbd2b80ac21fd9b8fca0c
alline.cardoso <migowskialline@gmail.com>
2021-11-22 11:38:00 -0300

    refactor mer, banco, pedido, itens pedido, dto

--- src/main/java/com/app_pedidos/model/dto/PedidoDTO.java
+++ src/main/java/com/app_pedidos/model/dto/PedidoDTO.java
@@ -1,6 +1,7 @@
 package com.app_pedidos.model.dto;
 
 import com.app_pedidos.model.entity.Cliente;
+import com.app_pedidos.model.entity.ItensPedido;
 import com.app_pedidos.model.entity.Pedido;
 import lombok.Getter;
 
@@ -14,15 +15,17 @@ public class PedidoDTO {
     private LocalDate data;
     private Cliente cliente;
     private boolean situacao;
+    private ItensPedido itensPedido;
 
-    public PedidoDTO(Pedido pedido){
+    public PedidoDTO(Pedido pedido) {
         this.id = pedido.getId();
         this.data = pedido.getData();
         this.cliente = pedido.getCliente();
         this.situacao = pedido.isSituacao();
+        this.itensPedido = pedido.getItensPedido();
     }
 
-    public static List<PedidoDTO> converter(List<Pedido> pedidos){
+    public static List<PedidoDTO> converter(List<Pedido> pedidos) {
         return pedidos.stream().map(PedidoDTO::new).collect(Collectors.toList());
     }
 }
