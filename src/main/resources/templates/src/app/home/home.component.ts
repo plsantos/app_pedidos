4a3297d99630e045406a68400fd36f8d51d631b6
byanca <byanca.calixto@dcx.ufpb.br>
2021-11-25 13:38:06 -0300

    adicionado elemento de paginacao, alteracao inserts de produto

--- src/main/resources/templates/src/app/home/home.component.ts
+++ src/main/resources/templates/src/app/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Produto } from '../model/produto';
 import { ProductService } from './../services/product.service';
-import { HttpClient } from '@angular/common/http';
-
 
 
 @Component({
