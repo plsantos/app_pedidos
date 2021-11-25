54c2ecf539b45c5af7fa63216cc5a53641dbb7cf
Caroline Souza <loh_jg@hotmail.com>
2021-11-24 22:05:59 -0300

    crud endereco

--- src/main/java/com/app_pedidos/controller/EnderecoController.java
+++ src/main/java/com/app_pedidos/controller/EnderecoController.java
@@ -1,25 +1,14 @@
 package com.app_pedidos.controller;
 
 import com.app_pedidos.model.dto.EnderecoDTO;
-import com.app_pedidos.model.entity.Endereco;
-import com.app_pedidos.model.repositories.EnderecoRepository;
-import com.app_pedidos.model.services.ClienteService;
 import com.app_pedidos.model.services.EnderecoService;
-import com.app_pedidos.model.services.exceptions.DatabaseException;
-import com.app_pedidos.model.services.exceptions.ResourceNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping("/endereco")
@@ -33,5 +22,34 @@ public class EnderecoController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri(); //inserindo e repondendo no cabeçalho de resposta
 		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<EnderecoDTO>>findAll(){
+		List<EnderecoDTO>list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<EnderecoDTO>findById(@PathVariable Long id){
+		EnderecoDTO dto = service.findById(id);
+		
+		return ResponseEntity.ok().body(dto);//resposta 200 ou seja foi com sucesso
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<EnderecoDTO> update(@PathVariable Long id,@RequestBody EnderecoDTO dto){
+		dto = service.update(id,dto);
+		return ResponseEntity.ok().body(dto);
+		
+		
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+		
 	}
 }
